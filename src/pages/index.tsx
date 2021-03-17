73107ea5903ceb959f306d4773ad6750f337d17a
EzequielBurg <ezekiel_sci@hotmail.com>
2021-03-16 22:12:02 -0300

    server side rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,38 @@
+import { GetServerSideProps } from 'next';
 import { Title } from '../styles/pages/Home';
 
-export default function Home() {
-  return (
-    <Title>hello world</Title>
-  )
+interface IProduct {
+  id: string;
+  title: string;
 }
+
+interface HomeProps {
+  recommendedProducts: IProduct[];
+}
+
+export default function Home({ recommendedProducts }: HomeProps) {
+  return (
+    <div>
+      <section>
+        <Title>Products</Title>
+
+        <ul>
+          {recommendedProducts.map(item => (
+            <li key={item.id}>{item.title}</li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch('http://localhost:3333/recommended');
+  const recommendedProducts = await response.json();
+  
+  return {
+    props: {
+      recommendedProducts
+    }
+  }
+};
